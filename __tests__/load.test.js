2e156e844dc2a0e0e235f4a69c0e0a9d09f35045
Nael Louis <nael.louis1002@gmail.com>
2021-10-18 23:06:20 -0400

    Add the 3 tests for load.js

---
+++ __tests__/load.test.js
@@ -0,0 +1,25 @@
+const load = require('../api/load.js');
+
+test('file found and parsed sucessfully', async () => {
+    let data = await load('./__tests__/testfile.json');
+    let expected = "A rechargeable device for storing electrical energy in the form of chemical energy, consisting of one or more separate secondary cells.\\n(Source: CED)";
+    await expect(data.accumulator[0]).toMatch(expected);
+})
+
+test('file that is not in json format ', async () => {
+    expect.assertions(1);
+    try {
+        await load('./__tests__/test.txt');
+    } catch (e) {
+        expect(e.message).toMatch('Unexpected');
+    }
+})
+
+test('file that is not in json format ', async () => {
+    expect.assertions(1);
+    try {
+        await load('./__tests__/newtest.txt');
+    } catch (e) {
+        expect(e.message).toMatch('no such');
+    }
+})
