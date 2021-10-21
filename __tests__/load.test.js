db1a1d518671268c928830d9622ef1b69babbea4
Nael Louis <nael.louis1002@gmail.com>
2021-10-21 15:51:33 -0400

    Make sure there's no eslint warning

--- __tests__/load.test.js
+++ __tests__/load.test.js
@@ -3,7 +3,7 @@ const load = require('../api/load.js');
 test('file found and parsed sucessfully', async () => {
     let data = await load('./__tests__/testfile.json');
     let expected = "A rechargeable device for storing electrical energy in the form of chemical energy, consisting of one or more separate secondary cells.\\n(Source: CED)";
-    await expect(data.accumulator[0]).toMatch(expected);
+    expect(data.accumulator[0]).toMatch(expected);
 })
 
 test('file that is not in json format ', async () => {
