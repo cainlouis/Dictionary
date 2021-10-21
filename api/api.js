133da030de26f2a66240fbc8001153fbb16bbdca
Nael Louis <nael.louis1002@gmail.com>
2021-10-20 20:17:39 -0400

    Add comments

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
-
+//Nael Louis, 1934115
+/**
+ * Function to create server and return all definition of the word the user entered from the json 
+ * @param {*} json data.json received from load function
+ */
 function startServer(json) {
     //import `http` module with CJS
     const http = require("http");
@@ -12,12 +16,16 @@ function startServer(json) {
         //the second parameter indicates the base URL
         //required if we don't know if the url is relative
         const url = new URL(req.url, `http://${req.headers.host}`);
-        let wordSearchParam = url.searchParams.get('word');
+        //get word from the url 
+        let word = url.searchParams.get('word');
+        //if it returns a null value set header to 404
         if (wordSearchParam !== null) {
-            let word = url.searchParams.get('word');
+            //check if the value the user entered exist in json
             if (!json.hasOwnProperty(word)) {
+                //if not try the lowerCase equivalent
                 word = url.searchParams.get('word').toLowerCase();
             }
+            //if the value exist in json
             if (json.hasOwnProperty(word)) {
                 //set header to avoid Cross-Origin-Resource-Sharing errors 
                 res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
@@ -26,7 +34,9 @@ function startServer(json) {
                 //write the definition at 0
                 res.write(JSON.stringify(json[word]));
             } else {
+                //set header to avoid Cross-Origin-Resource-Sharing errors 
                 res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+                //write 404 for the header
                 res.writeHead(404, { 'Error': 'No such word in dictionary.' });
             }
         } else {
@@ -40,6 +50,9 @@ function startServer(json) {
     server.listen(3000);
 }
 
+/**
+ * IIFE to get the json before creating the server and listening to the port
+ */
 (async () => {
     //import `load` function with CJS
     const load = require("../api/load.js");
